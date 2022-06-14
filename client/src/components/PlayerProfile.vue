1d2ef1814cfcf3d440c6b1f9b481bae179af086f
Colin <crrood@gmail.com>
2022-06-14 11:28:30 -0700

    add personality, styling to player profile

--- client/src/components/PlayerProfile.vue
+++ client/src/components/PlayerProfile.vue
@@ -1,31 +1,41 @@
 <template>
-  <div class="grid grid-cols-12">
+  <div class="grid grid-cols-12 bg-parchment">
     <!-- Character Photo -->
-    <div class="col-span-4 flex justify-center h-96 bg-gray-300">
+    <div class="col-span-4 flex justify-center h-96">
       <img class="rounded-full max-w-full" :src='state.character.photo.fullsize'>
     </div>
-    <div class="col-span-8 grid grid-cols-12 grid-rows-6 bg-gray-100">
-      <p class="col-span-12 row-span-2 font-beyond-wonderland text-center text-7xl bg-gray-400">{{
+    <div class="col-span-8 grid grid-cols-12 grid-rows-6">
+      <p class="col-span-12 row-span-2 font-beyond-wonderland text-center text-7xl">{{
         state.character.name}}</p>
       <!-- Physical Details -->
       <div class="col-span-12 row-span-4 flex flex-wrap gap-x-12 justify-evenly p-5">
-        <label v-for="detail in Object.keys(state.character.details)" :key="detail">
-          <span class="">{{ capitalize(detail) }}: </span>
-          <input type="text" class="border-0 border-b-2 border-gray-200 focus:border-black focus:ring-0" size=15
-            v-model="state.character.details[detail]">
-        </label>
+        <span v-for="(value, name) in state.character.details" :key="name">
+          <label>{{ capitalize(name) }}: </label>
+          <input type="text"
+            class="" size=15
+            v-model="state.character.details[name]">
+        </span>
       </div>
-      <!-- Stats -->
     </div>
+    <!-- Stats -->
     <div class="col-span-12 flex justify-evenly gap-x-12 bg-blue-100">
       <span v-for="(value, name) in state.character.stats" class="border border-black text-center bg-blue-200">
-        <input type="text" class="border-0 border-b-2 border-gray-200 text-center focus:border-black focus:ring-0"
+        <input type="number" class="border-0 border-b-2 border-gray-200 text-center focus:border-black focus:ring-0"
           max-length=2 size=2 v-model="state.character.stats[name]">
         <hr>
-        {{ capitalize(name) }}
+        <label>{{ capitalize(name) }}</label>
+      </span>
+    </div>
+    <!-- Personality and beliefs -->
+    <div class="col-span-12 grid grid-cols-12 gap-4 p-3 bg-blue-200">
+      <span v-for="(value, name) in state.character.personality" class="col-span-4 bg-blue-300">
+        <label class="">{{ capitalize(name) }}: </label>
+        <textarea class="resize-none border-0 border-b-2 border-gray-200 text-center focus:border-black focus:ring-0"
+          v-model="state.character.personality[name]"></textarea>
       </span>
     </div>
   </div>
+
   <!-- Save / load -->
   <div class="flex justify-center py-2 gap-x-4">
     <button class="btn-primary" @click='getCharacter'>Thump in!</button>
@@ -38,27 +48,10 @@ import { inject, reactive } from 'vue';
 
 // reactive state
 const state = reactive({
-  id: "62a820b4b1863f5f3b636119", // manually set after /resetDB
+  id: "62a8cd5ab05e4f23a07d2760", // manually set after /resetDB
   // placeholder data while page is loading
   character: {
     name: 'Placeholder name',
-    details: {
-      "race": "",
-      "class": "",
-      "background": "",
-      "level": 1,
-      "gender": "",
-      "height": "",
-      "weight": ""
-    },
-    "stats": {
-      "strength": 0,
-      "dexterity": 0,
-      "constitution": 0,
-      "intelligence": 0,
-      "wisdom": 0,
-      "charisma": 0
-    },
     photo: {
       fullsize: 'https://placekitten.com/250/384'
     }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'parchment': '#D6CCA9'
+      }
+    },
   },
   plugins: [],
 }
