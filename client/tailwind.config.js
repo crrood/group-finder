a08b4a22c1eca68e69accf71c033a6f12adc76f0
Colin <crrood@gmail.com>
2022-07-08 10:14:31 -0700

    change primary color to black

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
       colors: {
         'parchment': '#cf9e5e',
         'parchment-dark': '#cb9043',
-        'primary': '#1E293B', // slate-800
+        'primary': '#000000', // slate-800
         'secondary': '#deb887',
         'accent': '#6B21A8', // purple-800
         'accent-dark': '#581C87',  // purple-900
